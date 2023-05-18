5ca1b8950819a7000d0dfd83fdfc6ec58327239c
sanjaysudagani <divinespiritualforest@gmail.com>
2023-05-18 15:11:58 +0530

    first commit

---
+++ src/app/hero.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export interface Hero {
+  id: number;
+  name: string;
+}
